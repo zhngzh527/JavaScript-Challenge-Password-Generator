7d97b1f672ba4395412e669265a94a48c9692f38
ZHONGCHENG ZHAO <zhongchengzhao@ZHONGCHENGs-MBP.lan>
2022-10-03 22:24:44 -0500

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,76 @@
 // Assignment code here
+function generatePassword() {
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var specialCharacters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '[', ']', '{', '}', ';', ',', '<', '>', '/', '?', '`']
+  var possibleCharacters = [];
 
+ numberOfCharacters = prompt("How many characters do you want to use for your password? Choose between 8-128 characters.")
+ if ((numberOfCharacters < 8 || numberOfCharacters > 128) || isNaN(numberOfCharacters)) {
+ numberOfCharacters = prompt("How many characters do you want to use for your password? Choose between 8-128 characters.");
+ }
+ else {
+  alert("Your password will be " + numberOfCharacters + " characters long.");
+
+ }
+
+ hasLowercase = confirm("Do you want to include lowercase characters?");
+ if (hasLowercase) {
+  alert("Your password will included lowercase characters.");
+ }
+ else {
+  alert("Your password will not included lowercase characters.")
+ }
+
+ hasUppercase = confirm("Do you want to include uppercase characters?");
+ if (hasUppercase) {
+  alert("Your password will included uppercase characters.");
+ }
+ else {
+  alert("Your password will not included uppercase characters.")
+ }
+
+ hasNumbers = confirm("Do you want to include numbers?");
+ if (hasNumbers) {
+  alert("Your password will include numbers.");
+ }
+ else {
+  alert("Your password will not include numbers.");
+ }
+
+ hasSpecial = confirm("Do you want to include special characters?");
+ if (hasSpecial) {
+  alert("Your password will include special characters.");
+ }
+ else {
+  alert("Your password will not include special characters.")
+ }
+
+ if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
+  numberOfCharacters = prompt("How many characters do you want to use for your password? Choose between 8-128 characters.");
+ };
+
+ if (hasLowercase) {
+  possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
+ }
+ if (hasUppercase) {
+  possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
+ }
+ if (hasNumbers) {
+  possibleCharacters = possibleCharacters.concat(numbers);
+ }
+ if (hasSpecial) {
+  possibleCharacters = possibleCharacters.concat(specialCharacters);
+ }
+
+ let newPassword = ""
+ for (let i = 0; i < numberOfCharacters; i++) {
+  let rng =[Math.floor(Math.random() * possibleCharacters.length)];
+  newPassword = newPassword + possibleCharacters[rng];
+ }
+ return newPassword;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Password Generator Starter Code
+# Password Generator
+Module 3 Challenge
+## Description
+This site was designed to assist client to randomly generate a password that meets certain creiteria. [Link](https://zhngzh527.github.io/JavaScript-Challenge-Password-Generator)
+I have using this technologies to creat the web application:
+-JavaScript variable
+-JaveScript arrays
+-JavaScript functions
+-JaveScript Scope
+-JaveScript methods
+
+## Application Screenshot
+![alt text](Develop/Images/Screenshot 1.png)
+![alt text](Develop/Images/Screenshot 2.png)
